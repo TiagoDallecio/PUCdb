4cce117470b20915c89a6559f4cba4a45ab238f0
Tiago Oliveira Dallecio <112666497+TiagoDallecio@users.noreply.github.com>
2022-09-06 15:41:38 -0300

    Upload da tabela de alunos
    Upload do código de criação e preenchimento da tabela de alunos.

---
+++ DB_Alunos.sql
@@ -0,0 +1,25 @@
+CREATE TABLE ALUNOS (
+    RA INT,
+	NOME VARCHAR(40),
+	TURMA INT
+);
+
+INSERT INTO alunos (nome,ra,turma) VALUES ('TIAGO', 222, 1);
+
+INSERT INTO alunos (nome,ra,turma) VALUES ('JOÃO', 123, 1);
+
+INSERT INTO alunos (nome,ra,turma) VALUES ('MARIA', 276, 2);
+
+INSERT INTO alunos (nome,ra,turma) VALUES ('BRENDA', 65, 1);
+
+INSERT INTO alunos (nome,ra,turma) VALUES ('RENAN', 25, 2);
+
+INSERT INTO alunos (nome,ra,turma) VALUES ('PEDRO', 96, 1);
+
+INSERT INTO alunos (nome,ra,turma) VALUES ('JULIA', 145, 1);
+
+INSERT INTO alunos (nome,ra,turma) VALUES ('RICARDO', 278,2);
+
+INSERT INTO alunos (nome,ra,turma) VALUES ('ENZO', 111, 2);
+
+INSERT INTO alunos (nome,ra,turma) VALUES ('GABRIEL', 198, 2);
