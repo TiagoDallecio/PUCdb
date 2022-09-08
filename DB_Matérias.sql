a61e2726d7dc827a990b5c473ace57249e49b55f
Tiago Oliveira Dallecio <112666497+TiagoDallecio@users.noreply.github.com>
2022-09-08 12:05:12 -0300

    Atualização do banco de dados de matérias
    Melhora do banco de dados principal, com a inserção de Inner Joins e de tabelas complementares a fim de uma exibição mais completa das informações.

--- DB_Matérias.sql
+++ DB_Matérias.sql
@@ -1,51 +1,75 @@
-CREATE TABLE MATÉRIAS (
-	id_mat SERIAL NOT NULL,
-	nome_mat VARCHAR(65) NOT NULL,
-	RA INT NOT NULL,
-	RP INT NOT NULL,
- 	turma INT NOT NULL,
-	CONSTRAINT fk_ra FOREIGN KEY (RA) REFERENCES alunos (RA),
-	CONSTRAINT fk_rp FOREIGN KEY (RP) REFERENCES professores (RP)
+
+CREATE TABLE disciplinas (
+    id SERIAL PRIMARY KEY NOT NULL,
+	disciplina VARCHAR(60) NOT NULL
 );
 
-SELECT * FROM MATÉRIAS; 
+INSERT INTO disciplinas (disciplina) VALUES ('PI:DESENVOLVIMENTO WEB');
+INSERT INTO disciplinas (disciplina) VALUES ('PROGRAMAÇÃO DE COMPUTADORES');
+INSERT INTO disciplinas (disciplina) VALUES ('CÁLCULO 1');
+INSERT INTO disciplinas (disciplina) VALUES ('VIDA UNIVERSITÁRIA');
+INSERT INTO disciplinas (disciplina) VALUES ('ÁLGEBRA LINEAR');
 
-INSERT INTO MATÉRIAS (id_mat, nome_mat, RA, RP, turma) VALUES (1, 'PI:DESENVOLVIMENTO WEB', 222, 54, 1);
-INSERT INTO MATÉRIAS (id_mat, nome_mat, RA, RP, turma) VALUES (2,'PROGRAMAÇÃO DE COMPUTADORES', 222, 22, 1);
-INSERT INTO MATÉRIAS (id_mat, nome_mat, RA, RP, turma) VALUES (3,'CÁLCULO 1', 222, 98, 1);
+CREATE TABLE mat (
+	id_mat SERIAL,
+	RA INT,
+	RP INT,
+	turma INT,
+	
+	CONSTRAINT fk_ra  FOREIGN KEY (RA) REFERENCES alunos (ra),
+	CONSTRAINT fk_rp  FOREIGN KEY (RP) REFERENCES professores(rp),
+	CONSTRAINT fk_idmat FOREIGN KEY (id_mat) REFERENCES disciplinas (id)
+	
+	
+);
 
-INSERT INTO MATÉRIAS (id_mat, nome_mat, RA, RP, turma) VALUES (1,'PI:DESENVOLVIMENTO WEB', 123, 54, 1);
-INSERT INTO MATÉRIAS (id_mat, nome_mat, RA, RP, turma) VALUES (2,'PROGRAMAÇÃO DE COMPUTADORES ', 123, 22, 1);
-INSERT INTO MATÉRIAS (id_mat, nome_mat, RA, RP, turma) VALUES (3,'CÁLCULO 1', 123, 98, 1);
+INSERT INTO mat (id_mat, ra, rp, turma) VALUES (1, 222, 54, 1);
+INSERT INTO mat (id_mat, ra, rp, turma) VALUES (2, 222, 22, 1);
+INSERT INTO mat (id_mat, ra, rp, turma) VALUES (3, 222, 98, 1);
 
-INSERT INTO MATÉRIAS (id_mat, nome_mat, RA, RP, turma) VALUES (4,'VIDA UNIVERSITÁRIA', 276, 54, 2);
-INSERT INTO MATÉRIAS (id_mat, nome_mat, RA, RP, turma) VALUES (2,'PROGRAMAÇÃO DE COMPUTADORES', 276, 22, 2);
-INSERT INTO MATÉRIAS (id_mat, nome_mat, RA, RP, turma) VALUES (5,'ÁLGEBRA LINEAR', 276, 98, 2);
+INSERT INTO mat (id_mat, ra, rp, turma) VALUES (1, 123, 54, 1);
+INSERT INTO mat (id_mat, ra, rp, turma) VALUES (2, 123, 22, 1);
+INSERT INTO mat (id_mat, ra, rp, turma) VALUES (3, 123, 98, 1);
 
-INSERT INTO MATÉRIAS (id_mat, nome_mat, RA, RP, turma) VALUES (1,'PI:DESENVOLVIMENTO WEB', 65, 54, 1);
-INSERT INTO MATÉRIAS (id_mat, nome_mat, RA, RP, turma) VALUES (2,'PROGRAMAÇÃO DE COMPUTADORES', 65, 22, 1);
-INSERT INTO MATÉRIAS (id_mat, nome_mat, RA, RP, turma) VALUES (3,'CÁLCULO 1', 65, 98, 1);
+INSERT INTO mat (id_mat, ra, rp, turma) VALUES (4, 276, 54, 2);
+INSERT INTO mat (id_mat, ra, rp, turma) VALUES (2, 276, 22, 2);
+INSERT INTO mat (id_mat, ra, rp, turma) VALUES (5, 276, 98, 2);
 
-INSERT INTO MATÉRIAS (id_mat, nome_mat, RA, RP, turma) VALUES (4,'VIDA UNIVERSITÁRIA', 25, 54, 2);
-INSERT INTO MATÉRIAS (id_mat, nome_mat, RA, RP, turma) VALUES (2,'PROGRAMAÇÃO DE COMPUTADORES', 25, 22, 2);
-INSERT INTO MATÉRIAS (id_mat, nome_mat, RA, RP, turma) VALUES (5,'ÁLGEBRA LINEAR', 25, 98, 2);
+INSERT INTO mat (id_mat, ra, rp, turma) VALUES (1, 65, 54, 1);
+INSERT INTO mat (id_mat, ra, rp, turma) VALUES (2, 65, 22, 1);
+INSERT INTO mat (id_mat, ra, rp, turma) VALUES (3, 65, 98, 1);
 
-INSERT INTO MATÉRIAS (id_mat, nome_mat, RA, RP, turma) VALUES (1,'PI:DESENVOLVIMENTO WEB', 96, 54, 1);
-INSERT INTO MATÉRIAS (id_mat, nome_mat, RA, RP, turma) VALUES (2,'PROGRAMAÇÃO DE COMPUTADORES', 96, 22, 1);
-INSERT INTO MATÉRIAS (id_mat, nome_mat, RA, RP, turma) VALUES (3,'CÁLCULO 1', 96, 98, 1);
+INSERT INTO mat (id_mat, ra, rp, turma) VALUES (4, 25, 54, 2);
+INSERT INTO mat (id_mat, ra, rp, turma) VALUES (2, 25, 22, 2);
+INSERT INTO mat (id_mat, ra, rp, turma) VALUES (5, 25, 98, 2);
 
-INSERT INTO MATÉRIAS (id_mat, nome_mat, RA, RP, turma) VALUES (1,'PI:DESENVOLVIMENTO WEB', 145, 54, 1);
-INSERT INTO MATÉRIAS (id_mat, nome_mat, RA, RP, turma) VALUES (2,'PROGRAMAÇÃO DE COMPUTADORES', 145, 22, 1);
-INSERT INTO MATÉRIAS (id_mat, nome_mat, RA, RP, turma) VALUES (3,'CÁLCULO 1', 145, 98, 1);
+INSERT INTO mat (id_mat, ra, rp, turma) VALUES (1, 96, 54, 1);
+INSERT INTO mat (id_mat, ra, rp, turma) VALUES (2, 96, 22, 1);
+INSERT INTO mat (id_mat, ra, rp, turma) VALUES (3, 96, 98, 1);
 
-INSERT INTO MATÉRIAS (id_mat, nome_mat, RA, RP, turma) VALUES (4,'VIDA UNIVERSITÁRIA', 278, 54, 2);
-INSERT INTO MATÉRIAS (id_mat, nome_mat, RA, RP, turma) VALUES (2,'PROGRAMAÇÃO DE COMPUTADORES', 278, 22, 2);
-INSERT INTO MATÉRIAS (id_mat, nome_mat, RA, RP, turma) VALUES (5,'ÁLGEBRA LINEAR', 278, 98, 2);
+INSERT INTO mat (id_mat, ra, rp, turma) VALUES (1, 145, 54, 1);
+INSERT INTO mat (id_mat, ra, rp, turma) VALUES (2, 145, 22, 1);
+INSERT INTO mat (id_mat, ra, rp, turma) VALUES (3, 145, 98, 1);
 
-INSERT INTO MATÉRIAS (id_mat, nome_mat, RA, RP, turma) VALUES (4,'VIDA UNIVERSITÁRIA', 111, 54, 2);
-INSERT INTO MATÉRIAS (id_mat, nome_mat, RA, RP, turma) VALUES (2,'PROGRAMAÇÃO DE COMPUTADORES', 111, 22, 2);
-INSERT INTO MATÉRIAS (id_mat, nome_mat, RA, RP, turma) VALUES (5,'ÁLGEBRA LINEAR', 111, 98, 2);
+INSERT INTO mat (id_mat, ra, rp, turma) VALUES (4, 278, 54, 2);
+INSERT INTO mat (id_mat, ra, rp, turma) VALUES (2, 278, 22, 2);
+INSERT INTO mat (id_mat, ra, rp, turma) VALUES (5, 278, 98, 2);
 
-INSERT INTO MATÉRIAS (id_mat, nome_mat, RA, RP, turma) VALUES (4,'VIDA UNIVERSITÁRIA', 198, 54, 2);
-INSERT INTO MATÉRIAS (id_mat, nome_mat, RA, RP, turma) VALUES (2,'PROGRAMAÇÃO DE COMPUTADORES', 198, 22, 2);
-INSERT INTO MATÉRIAS (id_mat, nome_mat, RA, RP, turma) VALUES (5,'ÁLGEBRA LINEAR', 198, 98, 2);
+INSERT INTO mat (id_mat, ra, rp, turma) VALUES (4, 111, 54, 2);
+INSERT INTO mat (id_mat, ra, rp, turma) VALUES (2, 111, 22, 2);
+INSERT INTO mat (id_mat, ra, rp, turma) VALUES (5, 111, 98, 2);
+
+INSERT INTO mat (id_mat, ra, rp, turma) VALUES (4, 198, 54, 2);
+INSERT INTO mat (id_mat, ra, rp, turma) VALUES (2, 198, 22, 2);
+INSERT INTO mat (id_mat, ra, rp, turma) VALUES (5, 198, 98, 2);
+
+
+
+SELECT mat.id_mat, disciplinas.disciplina,alunos.ra, alunos.nome, professores.rp, professores.nome, alunos.turma
+FROM disciplinas
+INNER JOIN mat
+ON mat.id_mat = disciplinas.id
+INNER JOIN alunos
+ON alunos.ra = mat.ra
+INNER JOIN professores
+ON professores.rp = mat.rp;
